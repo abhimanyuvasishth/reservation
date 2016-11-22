4e7254b75e149cb27ef455a875ab36b96a74732c
abhimanyu <abhimanyu.vasishth@gmail.com>
2016-11-22 20:24:40 +0400

    skeleton code commit

---
+++ scripts/main.js
@@ -0,0 +1,12 @@
+var x = 10;
+var y = 10;
+var rad = 10;
+
+function setup(){
+	var canvas = createCanvas(windowWidth, windowHeight);
+}
+
+function draw(){
+	background(100);
+	ellipse(x,y,rad,rad);
+}
